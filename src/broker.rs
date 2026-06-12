0e7d4481b4b23d3e32b7472e8271e68219119680
Gautam011004 <divyagautam326@gmail.com>
2026-06-12 23:23:45 +0530

    Added consumer groups for offset tracking

--- src/broker.rs
+++ src/broker.rs
@@ -14,13 +14,19 @@ pub struct Topic {
 }
 
 pub struct Broker {
-    pub topics: HashMap<String, Topic>
+    pub topics: HashMap<String, Topic>,
+    pub groups: HashMap<String, ConsumerGroup>
+}
+
+pub struct ConsumerGroup {
+    pub offsets: HashMap<String, u64>
 }
 
 impl Broker {
     pub fn new() -> Self {
         Self {
-            topics: HashMap::<String, Topic>::new()
+            topics: HashMap::<String, Topic>::new(),
+            groups: HashMap::<String, ConsumerGroup>::new()
         }
     }
     pub fn create_topic(&mut self, name: &str) {
@@ -31,24 +37,31 @@ impl Broker {
             storage: storage
         };
         self.topics.insert(name.to_string(), v);
+        self.topics.get_mut(name).unwrap().messages = self.topics.get_mut(name).unwrap().storage.load();
     }
     pub fn publish(&mut self, topic: &str, payload: String) {
         let v = self.topics.get_mut(topic).unwrap();
         let logs = &mut v.storage;
-        let s=  match v.messages.last(){
+        let s =  match v.messages.last(){
             Some(m) => {
-                m.offset
+                m.offset + 1
             }
             None => {
                 0
             }
         };
         let new = Message {
-            offset: s+1,
+            offset: s,
             payload
         };
         v.messages.push(new.clone());
         logs.append(&new);
+    }
+    pub fn create_group(&mut self, topic: String) {
+        let new = ConsumerGroup { 
+            offsets: HashMap::new()
+        };
+        self.groups.entry(topic).or_insert_with(|| new);
     }
     pub fn consume(&self, offset: u64, topic: &str) -> Vec<Message> {
         let v = &self.topics.get(topic).unwrap().messages;
@@ -57,10 +70,9 @@ impl Broker {
         }
         v[offset as usize..].to_vec()
     }
-    pub fn remake(&mut self) {
-        for i in &mut self.topics {
-            let s = i.1.storage.load();
-            i.1.messages = s;
-        }
+    pub fn commit(&mut self, topic: String, consumer: String, commit: u64) {
+        let v = self.groups.get_mut(&topic).unwrap();
+        let s = v.offsets.entry(consumer).or_default();
+        *s = commit;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,13 @@ fn main() {
     let mut v = Broker::new();
     v.create_topic("orders");
     v.create_topic("transactions");
-    v.create_topic("liqidations");
-    v.remake();
-    let o = &v.topics.get("orders").unwrap().messages;
-    let t = &v.topics.get("transactions").unwrap().messages;
-    let l = &v.topics.get("liqidations").unwrap().messages;
-    println!("{:?}, \n {:?}, \n {:?}", o, t, l);
+    v.create_topic("liquidation");
+    v.create_group("orders".to_string());
+    v.publish("orders", "BUY 1 BTC".to_string());
+    v.publish("orders", "BUY 2 BTC".to_string());
+    v.publish("orders", "SELL 1 BTC".to_string());
+    v.publish("liquidation", "1 BTC DONE".to_string());
+    v.publish("liquidation", "2 BTC DONE".to_string());
+    v.commit("orders".to_string(), "analytics".to_string(), 2);
+    println!("{:?}, \n", v.groups.get("orders").unwrap().offsets.get("analytics").unwrap());
 }
